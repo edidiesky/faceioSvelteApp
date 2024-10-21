ae166383a2c2becfdcbcc022cbdc2dd3c4341ac5
edidiesky <essienedidiong1000@gmail.com>
2024-10-21 01:55:23 -0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+  import "../app.css";
+</script>
+
+<main class="bg-[#F4F4F9] min-h-[100vh]">
+  <!-- Header -->
+  <div class="h-[70px] flex items-center w-full bg-[#fff] shadow">
+    <div
+      class="w-full max-w-[1200px] px-8 mx-auto flex items-center justify-between"
+    >
+      <h3 class="font-bold text-2xl">
+        Miltz<span class="font-normal">bank</span>
+      </h3>
+
+      <ol class="hidden md:flex items-center justify-end gap-12">
+        <li class="text-base">Features</li>
+        <li class="text-base">Pricing</li>
+        <li class="text-base">About</li>
+      </ol>
+    </div>
+  </div>
+  <div
+    class="w-full max-w-[1200px] px-8 mx-auto min-h-[600px] flex gap-12 flex-col md:items-center justify-center"
+  >
+    <h2 class="text-5xl md:text-7xl md:text-center font-bold">
+      Swift transfers with Milzbank
+      <span
+        class="font-normal text-base max-w-[600px] md:mx-auto block md:text-xl pt-6"
+      >
+        Send money across borders instantly with our cutting-edge currency
+        exchange platform.
+      </span>
+    </h2>
+    <div class="flex md:items-center md:justify-center">
+      <button
+        class="px-8 py-3 hover:opacity-40 font-semibold text-lg rounded-lg bg-[#00BFA6] text-white flex items-center justify-center gap-4"
+      >
+        Sign Up Now
+      </button>
+    </div>
+  </div>
+</main>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+</script>
+
+<main class="bg-[#F4F4F9] min-h-[100vh]">
+  <div
+    class="w-full max-w-[1200px] px-8 mx-auto min-h-[600px] flex gap-12 flex-col md:items-center justify-center"
+  >
+    <div
+      class="w-full p-6 bg-white md:w-[560px] shadow rounded-xl flex flex-col gap-8"
+    >
+      <h2 class="text-3xl md:text-4xl md:text-center font-bold">
+        Milzbank
+        <span
+          class="font-bold text-xl max-w-[600px] md:mx-auto block md:text-2xl pt-6"
+        >
+          Swift transfers with Milzbank
+        </span>
+      </h2>
+      <div class="flex md:items-center md:justify-center">
+        <button
+          class="px-8 py-3 hover:opacity-40 font-semibold text-lg rounded-lg bg-[#00BFA6] text-white flex items-center justify-center gap-4"
+        >
+          Sign Up Now
+        </button>
+      </div>
+    </div>
+  </div>
+</main>
